f71a24643d7d49ccdfe8eb187ae8da5aebf62e7f
SpartiK <44985390+SpartiK@users.noreply.github.com>
2018-12-14 17:41:38 +0200

    Класс

--- src/OOP/PhoneDemo.java
+++ src/OOP/PhoneDemo.java
@@ -5,14 +5,26 @@ import java.sql.SQLOutput;
 public class PhoneDemo {
     public static void main(String[] args) {
         Phone phone1 = new Phone();
-        Phone phone2 = new Phone();
+        Phone phone2 = new Phone("+380951586666", "Motorolla", 250);
         Phone phone3 = new Phone();
-
+        Phone phone4 = new Phone("+3809515865555", "Nokia");
         phone1.number = "+380951586789";
         phone1.model = "Apple";
         phone1.weight = 175.4;
 
 
-        System.out.println(phone1.number+" "+phone1.model+" "+phone1.weight);
+        System.out.println(phone1.number + " " + phone1.model + " " + phone1.weight);
+        System.out.println(phone2.number + " " + phone2.model + " " + phone2.weight);
+        System.out.println(phone3.number + " " + phone3.model + " " + phone3.weight);
+        System.out.println(phone4.number + " " + phone4.model + " " + phone4.weight);
+
+
+        phone1.receiveCall("Вова");
+        phone1.receiveCall("Вова", "+380990000762");
+
+
+        System.out.println(phone1.getNumber());
+
+
     }
 }
