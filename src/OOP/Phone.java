e188837b4f2f8dc7461479d319b4ffe2b271c93f
SpartiK <44985390+SpartiK@users.noreply.github.com>
2018-12-14 17:41:31 +0200

    Класс

--- src/OOP/Phone.java
+++ src/OOP/Phone.java
@@ -5,6 +5,34 @@ public class Phone {
     String model;
     double weight;
 
+    public Phone() {
+    }
+
+    public Phone(String number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone(String number, String model, double weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    void receiveCall(String name) {
+        System.out.println("Звонит " + name);
+
+    }
+
+    void receiveCall(String name, String number) {
+        System.out.println("Звонит " + name + " - " + number);
+        System.out.println("Текущий телефон " + this.number);
+
+    }
+
+    String getNumber() {
+        return number;
+    }
 
 
 }
